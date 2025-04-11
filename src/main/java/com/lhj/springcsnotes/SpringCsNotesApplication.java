4da59f940164951ea249a7dadc520ce6e4c98e45
hyungjae.lee <dlgudwo11@gmail.com>
2025-04-11 14:15:56 +0900

    Spring-cs-notes
    - init commit

---
+++ src/main/java/com/lhj/springcsnotes/SpringCsNotesApplication.java
@@ -0,0 +1,13 @@
+package com.lhj.springcsnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCsNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCsNotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lhj/springcsnotes/SpringCsNotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lhj.springcsnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCsNotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
